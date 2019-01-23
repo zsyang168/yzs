b866efb08179b7ee9cd031b82a2538af5c8113ae
yang.zisong <yang.zisong@embedway.com>
2019-01-23 09:32:56 +0800

    add wiki

---
+++ wiki/conf/users.auth.php
@@ -0,0 +1,13 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+yzs:$1$MzDlQQFe$I9k12LdThFG7OTqux/utj0:杨子松:1377477833@qq.com:admin,user
+wxd:$1$tb57E6b3$H5oqm/DhJf58OFj1xampN1:王绪东:wxd@qq.com:admin,user
